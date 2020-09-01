755a4a1298284b5414e50bb86efd9b5c0866db00
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-01 11:05:45 +0200

    updatefix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const app = express();
 app.use(morgan("common"));
 // Adding helmet to hide what backend is being used.
 app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello there!",
+  });
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+app.use((error, res, req, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
