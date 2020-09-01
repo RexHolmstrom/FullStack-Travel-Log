56f7758233cfe2d217cb1213e388871f8e4f383e
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-01 09:50:20 +0200

    backend protector added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
+app.use(morgan("common"));
+// Adding helmet to hide what backend is being used.
+app.use(helmet());
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
