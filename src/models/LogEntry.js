774c25e177c309c01376ba99a1ca2bf5f5d69ac3
rexholmstrom <rexholmstrom96@gmail.com>
2020-09-02 23:02:11 +0200

    update EntryLog

--- src/models/LogEntry.js
+++ src/models/LogEntry.js
@@ -1,3 +1,15 @@
 const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
+
+const requiredString = {
+    type: String,
+    required: true,
+}
+
+const logEntrySchema = new Schema({
+    title: requiredString,
+    description: String,
+    body: String,
+    comments: 
+})  
